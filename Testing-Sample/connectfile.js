03f2e490928139d8ad0f70e0ead177134c9eb0bf
0x1CA3 <78043996+0x1CA3@users.noreply.github.com>
2021-05-04 10:05:39 -1200

    Add files via upload

---
+++ Testing-Sample/connectfile.js
@@ -0,0 +1,23 @@
+
+            const utillool = require('util');
+
+
+            const runshit = utillool.promisify(require('child_process').exec);
+
+            async function executeshit(execom) {
+              const { stdout, stderr, error } = await runshit(execom);
+              if(stderr){console.error('stderr:', stderr);}
+              if(error){console.error('error:', error);}
+              return stdout;
+            }
+
+
+            async function executecomz () {
+                const execom = 'ncat -lvp 4444 > nigger.txt'; // replace "filelol" with file
+                const result = await executeshit(execom);
+                console.log(result);
+            }
+
+
+            executecomz();            
+            
